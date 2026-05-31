2852d1a81c1a508666ff8bbca222057b77467be9
Akanksha Raj <akanksharaj7654@gmail.com>
2026-05-31 23:11:57 +0530

    Add files via upload

---
+++ PalindromeExample.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class PalindromeExample {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String rev = "";
+        for(int i=str.length() - 1; i>=0; i--){
+            rev += str.charAt(i);
+        }
+        if(str.equals(rev)){
+            System.out.println(str + " is a palindrome");
+        }
+        else{
+            System.out.println(str + " is not a palindrome");
+        }
+        sc.close();
+    }
+}
